be5a939f0ddf05f36f20831c49d4b0cf822b2fb5
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-08 12:06:08 +0200

    Remove superseded early runtime patch hook

--- src/v6/__init__.py
+++ src/v6/__init__.py
@@ -1,8 +1,5 @@
 """ARC-AGI3 v6 cognitive interaction learner."""
 
 from v6.main import V6Config, V6System
-from v6.runtime_compat import install_runtime_compatibility
-
-install_runtime_compatibility()
 
 __all__ = ["V6Config", "V6System"]
