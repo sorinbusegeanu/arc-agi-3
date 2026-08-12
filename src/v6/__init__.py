af45c83fb8247e3c4bad2ddd960cfb083784ff66
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-12 13:22:35 +0200

    Install v0.4.2 cognitive policy

--- src/v6/__init__.py
+++ src/v6/__init__.py
@@ -4,9 +4,11 @@ from v6.main import V6Config, V6System
 from v6.hypothesis_suite_performance import install_hypothesis_suite_performance_policy
 from v6.hypothesis_suite_performance_compat import apply_hypothesis_suite_performance_compatibility
 from v6.h08_world_model_prediction_repair import install_h08_world_model_prediction_repair
+from v6.v042_policy import install_v042_policy
 
 install_hypothesis_suite_performance_policy()
 apply_hypothesis_suite_performance_compatibility()
 install_h08_world_model_prediction_repair()
+install_v042_policy()
 
 __all__ = ["V6Config", "V6System"]
