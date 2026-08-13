ed862036a9dc95fa94a908c004be9c5a5c55dcb8
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 12:31:51 +0200

    Export v7 derivation pipeline and executor

--- src/v7/derivation/__init__.py
+++ src/v7/derivation/__init__.py
@@ -1,12 +1,29 @@
+from v7.derivation.batches import DerivedMergeStats, DerivedMutationBatch, DeterministicDerivedBatchMerger
 from v7.derivation.dependencies import DependencyMutation, DirtyDerivationPlan, MemoryDependencyGraph
+from v7.derivation.executor import ParallelDerivationConfig, ParallelDerivationExecutor
+from v7.derivation.pipeline import HierarchyDerivationResult, MemoryLearningPipeline
+from v7.derivation.scientific import EpisodeEvidence, ScientificDerivationKernels
+from v7.derivation.vectorized import VectorizedDerivationEngine, VectorizedDerivationInput, VectorizedKernel
 from v7.derivation.workers import DerivationTask, DerivationTaskPlanner, DerivationTaskResult, DerivationWorker
 
 __all__ = [
     "DependencyMutation",
+    "DerivedMergeStats",
+    "DerivedMutationBatch",
     "DerivationTask",
     "DerivationTaskPlanner",
     "DerivationTaskResult",
     "DerivationWorker",
+    "DeterministicDerivedBatchMerger",
     "DirtyDerivationPlan",
+    "EpisodeEvidence",
+    "HierarchyDerivationResult",
     "MemoryDependencyGraph",
+    "MemoryLearningPipeline",
+    "ParallelDerivationConfig",
+    "ParallelDerivationExecutor",
+    "ScientificDerivationKernels",
+    "VectorizedDerivationEngine",
+    "VectorizedDerivationInput",
+    "VectorizedKernel",
 ]
