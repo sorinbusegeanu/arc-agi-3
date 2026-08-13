9f668aa21d985dfc1199af4c0113fc321b0ba256
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 10:04:37 +0200

    Add v7 cognition index package

---
+++ src/v7/memory/indexes/__init__.py
@@ -0,0 +1,21 @@
+from v7.memory.indexes.cognition import (
+    ActionAggregate,
+    ActionAggregateDelta,
+    ActionScoreInput,
+    CognitionIndexBuilder,
+    CognitionIndexes,
+    ContingencyIndexMutation,
+    RoleConceptIndexMutation,
+    RoleIndexMutation,
+)
+
+__all__ = [
+    "ActionAggregate",
+    "ActionAggregateDelta",
+    "ActionScoreInput",
+    "CognitionIndexBuilder",
+    "CognitionIndexes",
+    "ContingencyIndexMutation",
+    "RoleConceptIndexMutation",
+    "RoleIndexMutation",
+]
