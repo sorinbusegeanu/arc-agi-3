0b8ac43b8b45588c48937c63d1ac31a15b8f0174
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 12:30:05 +0200

    Install concept validation relevance pruning

--- src/v6/__init__.py
+++ src/v6/__init__.py
@@ -14,6 +14,7 @@ from v6.concept_validation_fastpath_compat import install_concept_validation_fas
 from v6.concept_validation_fastpath_fixups import install_concept_validation_fastpath_fixups
 from v6.concept_validation_sparse_cache import install_concept_validation_sparse_cache
 from v6.concept_validation_profiler_context_fix import install_concept_validation_profiler_context_fix
+from v6.concept_validation_relevance_pruning import install_concept_validation_relevance_pruning
 
 install_hypothesis_suite_performance_policy()
 apply_hypothesis_suite_performance_compatibility()
@@ -28,5 +29,6 @@ install_concept_validation_fastpath_compat()
 install_concept_validation_fastpath_fixups()
 install_concept_validation_sparse_cache()
 install_concept_validation_profiler_context_fix()
+install_concept_validation_relevance_pruning()
 
 __all__ = ["V6Config", "V6System"]
