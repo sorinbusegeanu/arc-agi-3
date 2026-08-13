265c69ac8b88d4ed1bc06a2a840250d0ca1afffd
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 10:13:50 +0200

    Export v7 read-view transports

---
+++ src/v7/memory/transport/__init__.py
@@ -0,0 +1,4 @@
+from v7.memory.transport.base import ReadViewHandle, ReadViewTransport
+from v7.memory.transport.local import LocalReadViewTransport
+
+__all__ = ["LocalReadViewTransport", "ReadViewHandle", "ReadViewTransport"]
