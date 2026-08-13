79acbe3e4747a105d7426800bceddf3ec1d58953
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 12:21:58 +0200

    Keep derivation package imports cycle-safe

--- src/v7/derivation/__init__.py
+++ src/v7/derivation/__init__.py
@@ -1,16 +1,12 @@
-from v7.derivation.batches import DerivedMergeStats, DerivedMutationBatch, DeterministicDerivedBatchMerger
 from v7.derivation.dependencies import DependencyMutation, DirtyDerivationPlan, MemoryDependencyGraph
-from v7.derivation.executor import ParallelDerivationConfig, ParallelDerivationExecutor
-from v7.derivation.pipeline import MemoryLearningPipeline
-from v7.derivation.scientific import EpisodeEvidence, ScientificDerivationKernels
-from v7.derivation.vectorized import VectorizedDerivationEngine, VectorizedDerivationInput
 from v7.derivation.workers import DerivationTask, DerivationTaskPlanner, DerivationTaskResult, DerivationWorker
 
 __all__ = [
-    "DependencyMutation", "DerivedMergeStats", "DerivedMutationBatch", "DerivationTask",
-    "DerivationTaskPlanner", "DerivationTaskResult", "DerivationWorker",
-    "DeterministicDerivedBatchMerger", "DirtyDerivationPlan", "EpisodeEvidence",
-    "MemoryDependencyGraph", "MemoryLearningPipeline", "ParallelDerivationConfig",
-    "ParallelDerivationExecutor", "ScientificDerivationKernels", "VectorizedDerivationEngine",
-    "VectorizedDerivationInput",
+    "DependencyMutation",
+    "DerivationTask",
+    "DerivationTaskPlanner",
+    "DerivationTaskResult",
+    "DerivationWorker",
+    "DirtyDerivationPlan",
+    "MemoryDependencyGraph",
 ]
