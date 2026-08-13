02c50a9b8916bde6ade36756c594de30fe7aed4b
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 12:21:46 +0200

    Export canonical and scoring contracts

--- src/v7/memory/__init__.py
+++ src/v7/memory/__init__.py
@@ -1,65 +1,31 @@
 from v7.memory.arenas import CompactMemoryArena, NodeColumns, PackedAdjacency, ScoreColumns
+from v7.memory.canonical import CanonicalCandidateMutation, CanonicalMemoryKey, CanonicalMemoryRegistry
 from v7.memory.coordinator import GenerationCommitCoordinator, GenerationCommitResult
 from v7.memory.delta import GenerationDelta
 from v7.memory.generation import GenerationId, GenerationState
 from v7.memory.ids import MemoryId, MemoryIdAllocator, MemoryLevel
-from v7.memory.indexes.cognition import (
-    ActionAggregate,
-    ActionAggregateDelta,
-    ActionScoreInput,
-    CognitionIndexBuilder,
-    CognitionIndexes,
-    ContingencyIndexMutation,
-    RoleConceptIndexMutation,
-    RoleIndexMutation,
+from v7.memory.indexes import (
+    ActionAggregate, ActionAggregateDelta, ActionScoreInput, CognitionIndexBuilder,
+    CognitionIndexes, ContingencyIndexMutation, PackedCognitionIndexes,
+    RoleConceptIndexMutation, RoleIndexMutation,
 )
 from v7.memory.models import EdgeMutation, EdgeState, MemoryNode, MemoryScore, NodeMutation, ScoreMutation
 from v7.memory.publisher import GenerationPublisher, PublicationRecord
 from v7.memory.read_view import MemoryReadView
-from v7.memory.transport import (
-    LocalReadViewTransport,
-    MmapReadViewTransport,
-    ReadViewHandle,
-    ReadViewTransport,
-    SegmentedMmapReadViewTransport,
-)
+from v7.memory.scoring import ActionScoreBatch, ActionScoringWeights, VectorizedActionScorer
+from v7.memory.transport import LocalReadViewTransport, MmapReadViewTransport, ReadViewHandle, ReadViewTransport, SegmentedMmapReadViewTransport
 from v7.memory.writer import CanonicalMemoryWriter, PreparedGeneration
 
 __all__ = [
-    "ActionAggregate",
-    "ActionAggregateDelta",
-    "ActionScoreInput",
-    "CanonicalMemoryWriter",
-    "CognitionIndexBuilder",
-    "CognitionIndexes",
-    "CompactMemoryArena",
-    "ContingencyIndexMutation",
-    "EdgeMutation",
-    "EdgeState",
-    "GenerationCommitCoordinator",
-    "GenerationCommitResult",
-    "GenerationDelta",
-    "GenerationId",
-    "GenerationPublisher",
-    "GenerationState",
-    "LocalReadViewTransport",
-    "MemoryId",
-    "MemoryIdAllocator",
-    "MemoryLevel",
-    "MemoryNode",
-    "MemoryReadView",
-    "MemoryScore",
-    "MmapReadViewTransport",
-    "NodeColumns",
-    "NodeMutation",
-    "PackedAdjacency",
-    "PreparedGeneration",
-    "PublicationRecord",
-    "ReadViewHandle",
-    "ReadViewTransport",
-    "RoleConceptIndexMutation",
-    "RoleIndexMutation",
-    "ScoreColumns",
-    "ScoreMutation",
-    "SegmentedMmapReadViewTransport",
+    "ActionAggregate", "ActionAggregateDelta", "ActionScoreBatch", "ActionScoreInput",
+    "ActionScoringWeights", "CanonicalCandidateMutation", "CanonicalMemoryKey",
+    "CanonicalMemoryRegistry", "CanonicalMemoryWriter", "CognitionIndexBuilder",
+    "CognitionIndexes", "CompactMemoryArena", "ContingencyIndexMutation", "EdgeMutation",
+    "EdgeState", "GenerationCommitCoordinator", "GenerationCommitResult", "GenerationDelta",
+    "GenerationId", "GenerationPublisher", "GenerationState", "LocalReadViewTransport",
+    "MemoryId", "MemoryIdAllocator", "MemoryLevel", "MemoryNode", "MemoryReadView",
+    "MemoryScore", "MmapReadViewTransport", "NodeColumns", "NodeMutation", "PackedAdjacency",
+    "PackedCognitionIndexes", "PreparedGeneration", "PublicationRecord", "ReadViewHandle",
+    "ReadViewTransport", "RoleConceptIndexMutation", "RoleIndexMutation", "ScoreColumns",
+    "ScoreMutation", "SegmentedMmapReadViewTransport", "VectorizedActionScorer",
 ]
