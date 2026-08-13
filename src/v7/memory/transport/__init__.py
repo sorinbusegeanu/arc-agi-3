6d869a0bde3b3cb2bdf27841fc5ce2612339800d
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 11:46:02 +0200

    Export mmap read-view transport

--- src/v7/memory/transport/__init__.py
+++ src/v7/memory/transport/__init__.py
@@ -1,4 +1,10 @@
 from v7.memory.transport.base import ReadViewHandle, ReadViewTransport
 from v7.memory.transport.local import LocalReadViewTransport
+from v7.memory.transport.mmap import MmapReadViewTransport
 
-__all__ = ["LocalReadViewTransport", "ReadViewHandle", "ReadViewTransport"]
+__all__ = [
+    "LocalReadViewTransport",
+    "MmapReadViewTransport",
+    "ReadViewHandle",
+    "ReadViewTransport",
+]
