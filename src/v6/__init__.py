0bb3320901fac32ba3f71e6f1d4b0e6d2ccf093a
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 06:17:16 +0200

    Install hypothesis suite runtime profiler

--- src/v6/__init__.py
+++ src/v6/__init__.py
@@ -8,6 +8,7 @@ from v6.v042_policy import install_v042_policy
 from v6.v042_compat import apply_v042_compatibility
 from v6.suite_runtime_repairs import install_suite_runtime_repairs
 from v6.suite_runtime_repairs_compat import install_suite_runtime_repairs_compat
+from v6.suite_runtime_profiler import install_suite_runtime_profiler
 
 install_hypothesis_suite_performance_policy()
 apply_hypothesis_suite_performance_compatibility()
@@ -16,5 +17,6 @@ install_v042_policy()
 apply_v042_compatibility()
 install_suite_runtime_repairs()
 install_suite_runtime_repairs_compat()
+install_suite_runtime_profiler()
 
 __all__ = ["V6Config", "V6System"]
