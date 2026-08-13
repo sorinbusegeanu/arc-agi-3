01c42e3008f59ce68b859264fdcea78aca47d4f2
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-13 12:21:22 +0200

    Export packed cognition indexes

--- src/v7/memory/indexes/__init__.py
+++ src/v7/memory/indexes/__init__.py
@@ -1,21 +1,14 @@
 from v7.memory.indexes.cognition import (
-    ActionAggregate,
-    ActionAggregateDelta,
-    ActionScoreInput,
-    CognitionIndexBuilder,
-    CognitionIndexes,
-    ContingencyIndexMutation,
-    RoleConceptIndexMutation,
-    RoleIndexMutation,
+    ActionAggregate, ActionAggregateDelta, ActionScoreInput, CognitionIndexBuilder,
+    CognitionIndexes, ContingencyIndexMutation, RoleConceptIndexMutation, RoleIndexMutation,
+)
+from v7.memory.indexes.packed import (
+    PackedActionAggregates, PackedCognitionIndexes, PackedPairIndex, PackedRoleExactIndex,
 )
 
 __all__ = [
-    "ActionAggregate",
-    "ActionAggregateDelta",
-    "ActionScoreInput",
-    "CognitionIndexBuilder",
-    "CognitionIndexes",
-    "ContingencyIndexMutation",
-    "RoleConceptIndexMutation",
-    "RoleIndexMutation",
+    "ActionAggregate", "ActionAggregateDelta", "ActionScoreInput", "CognitionIndexBuilder",
+    "CognitionIndexes", "ContingencyIndexMutation", "PackedActionAggregates",
+    "PackedCognitionIndexes", "PackedPairIndex", "PackedRoleExactIndex",
+    "RoleConceptIndexMutation", "RoleIndexMutation",
 ]
