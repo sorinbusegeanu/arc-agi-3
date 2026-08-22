5e7e693c50522d61194fc7b25da0c09a5791be99
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-22 10:42:01 +0200

    install v8.51 memory efficiency integrity completion

--- src/v8/runtime_stack_v88.py
+++ src/v8/runtime_stack_v88.py
@@ -63,6 +63,7 @@ _POST_LAYERS: tuple[str, ...] = (
     "learning_effectiveness_report_v850",
     "memory_efficiency_v851",
     "memory_efficiency_v851_fixups",
+    "memory_efficiency_v851_integrity",
 )
 
 
