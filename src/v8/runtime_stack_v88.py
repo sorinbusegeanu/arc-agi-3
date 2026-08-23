34ca64126b9891cafb09a9d56635aae4e45d2305
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-23 19:14:35 +0200

    install v8.57 click and transfer fixes

--- src/v8/runtime_stack_v88.py
+++ src/v8/runtime_stack_v88.py
@@ -113,4 +113,6 @@ def install_current_runtime_stack_v88() -> None:
     _installer("adaptive_memory_control_v855_fixups")()
     _installer("adaptive_memory_control_v855_final_fix")()
     _installer("trajectory_click_audit_v856")()
+    _installer("click_state_learning_v857")()
+    _installer("transfer_correspondence_v857")()
     _INSTALLED = True
