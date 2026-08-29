860b0df35baafee51d8cd3ffd70390a90a46994f
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-29 21:46:58 +0200

    install v8.60 click transition explorer

--- src/v8/runtime_stack_v88.py
+++ src/v8/runtime_stack_v88.py
@@ -115,4 +115,5 @@ def install_current_runtime_stack_v88() -> None:
     _installer("trajectory_click_audit_v856")()
     _installer("click_state_learning_v857")()
     _installer("transfer_correspondence_v857")()
+    _installer("click_transition_exploration_v860")()
     _INSTALLED = True
