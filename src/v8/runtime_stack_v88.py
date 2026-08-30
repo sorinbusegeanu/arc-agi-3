091d590e5e10793e634aa89de0e7b984b2328ffb
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-30 20:48:53 +0200

    install v8.79 information flow integrity layer

--- src/v8/runtime_stack_v88.py
+++ src/v8/runtime_stack_v88.py
@@ -147,4 +147,5 @@ def install_current_runtime_stack_v88() -> None:
     _installer("mixed_research_runtime_integrity_v876")()
     _installer("generic_result_flush_v877")()
     _installer("research_integrity_v878")()
+    _installer("information_flow_integrity_v879")()
     _INSTALLED = True
