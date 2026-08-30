6c8310b367a6a6eac0fa4916081e2ab11d3eb7c8
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-30 21:32:15 +0200

    test ARC list-backed sprite dimensions

--- src/v8/tests/test_v8_arcengine_render_compat_v880.py
+++ src/v8/tests/test_v8_arcengine_render_compat_v880.py
@@ -27,13 +27,32 @@ class ArcEngineRenderCompatibilityV880Tests(unittest.TestCase):
         self.assertIsInstance(rendered, np.ndarray)
         np.testing.assert_array_equal(rendered, np.asarray([[1, 2], [3, 4]], dtype=np.int8))
 
-    def test_compatibility_install_is_idempotent(self):
-        from arcengine.camera import Camera
+    def test_sprite_dimensions_accept_list_backed_render(self):
+        from arcengine.sprites import Sprite
+
+        class ListBackedSprite(Sprite):
+            def render(self):
+                return [[1, 2, 3], [4, 5, 6]]
 
         _install_arcengine_render_compatibility()
-        first = Camera._raw_render
+        sprite = object.__new__(ListBackedSprite)
+
+        self.assertEqual(sprite.width, 3)
+        self.assertEqual(sprite.height, 2)
+
+    def test_compatibility_install_is_idempotent(self):
+        from arcengine.camera import Camera
+        from arcengine.sprites import Sprite
+
         _install_arcengine_render_compatibility()
-        self.assertIs(Camera._raw_render, first)
+        first_camera = Camera._raw_render
+        first_width = Sprite.width.fget
+        first_height = Sprite.height.fget
+        _install_arcengine_render_compatibility()
+
+        self.assertIs(Camera._raw_render, first_camera)
+        self.assertIs(Sprite.width.fget, first_width)
+        self.assertIs(Sprite.height.fget, first_height)
 
 
 if __name__ == "__main__":
