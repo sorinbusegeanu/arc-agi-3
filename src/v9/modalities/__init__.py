3ec5ff74bf5fc355dd5e2b0b3958a5f8d96a07b4
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-08-30 21:23:50 +0200

    Expose v9 modality identities

---
+++ src/v9/modalities/__init__.py
@@ -0,0 +1,17 @@
+from v9.modalities.symbols import (
+    DeterministicSymbolCodec,
+    ModalityId,
+    SymbolId,
+    SymbolObservation,
+    SymbolStreamId,
+    SymbolVocabularyId,
+)
+
+__all__ = [
+    "DeterministicSymbolCodec",
+    "ModalityId",
+    "SymbolId",
+    "SymbolObservation",
+    "SymbolStreamId",
+    "SymbolVocabularyId",
+]
