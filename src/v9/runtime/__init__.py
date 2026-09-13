792698ec8649c8f32732d7b623c9363324a113bc
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-09-13 19:56:22 +0200

    v9: route runtime through optimized implementation

--- src/v9/runtime/__init__.py
+++ src/v9/runtime/__init__.py
@@ -1,6 +1,6 @@
 from .config import RuntimeConfig, ScientificConfig, ScientificConfigId
 from .publication import CanonicalGraph
 from .read_view import ReadView
-from .runtime import ContinuousMemoryRuntime
+from .optimized_runtime import ContinuousMemoryRuntime
 
 __all__ = ["CanonicalGraph", "ContinuousMemoryRuntime", "ReadView", "RuntimeConfig", "ScientificConfig", "ScientificConfigId"]
