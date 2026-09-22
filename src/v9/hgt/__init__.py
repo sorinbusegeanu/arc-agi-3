747f571f62ce895b066542313f1ae4eeb3b4a08c
sorinbusegeanu <80164929+sorinbusegeanu@users.noreply.github.com>
2026-09-23 00:10:57 +0200

    Export HGT policy loader

--- src/v9/hgt/__init__.py
+++ src/v9/hgt/__init__.py
@@ -1,4 +1,4 @@
-from .training import HGTTrainingResult, resolve_hgt_behavior_test, rollback_hgt_model, train_hgt_epoch
+from .training import HGTTrainingResult, load_hgt_policy_version, resolve_hgt_behavior_test, rollback_hgt_model, train_hgt_epoch
 from .training_cut import TrainingCut
 
-__all__ = ["HGTTrainingResult", "TrainingCut", "resolve_hgt_behavior_test", "rollback_hgt_model", "train_hgt_epoch"]
+__all__ = ["HGTTrainingResult", "TrainingCut", "load_hgt_policy_version", "resolve_hgt_behavior_test", "rollback_hgt_model", "train_hgt_epoch"]
